1749e5611f0da55e2ef7ba83aa74d59940b4f72c
francoism90 <francoism90@users.noreply.github.com>
2024-01-02 20:10:45 +0100

    wip

---
+++ src/Models/Concerns/WithAuthentication.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Foxws\LivewireUse\Models\Concerns;
+
+use Illuminate\Database\Eloquent\Model;
+
+trait WithAuthorization
+{
+
+}
--- src/Models/Concerns/WithAuthorization.php
+++ src/Models/Concerns/WithAuthorization.php
@@ -6,31 +6,36 @@ use Illuminate\Database\Eloquent\Model;
 
 trait WithAuthorization
 {
+    protected function can(string $ability, mixed $model): void
+    {
+        $this->authorize($ability, $model);
+    }
+
     protected function canViewAny(Model|string $model): void
     {
         if ($model instanceof Model) {
             $model = $model->getMorphClass();
         }
 
-        $this->authorize('viewAny', $model);
-    }
-
-    protected function canView(Model $model): void
-    {
         $this->can('viewAny', $model);
     }
 
-    protected function canCreate(Model $model): void
+    protected function canView(Model|array $model): void
+    {
+        $this->can('view', $model);
+    }
+
+    protected function canCreate(Model|array $model): void
     {
         $this->can('create', $model);
     }
 
-    protected function canUpdate(Model $model): void
+    protected function canUpdate(Model|array $model): void
     {
         $this->can('update', $model);
     }
 
-    protected function canDelete(Model $model): void
+    protected function canDelete(Model|array $model): void
     {
         $this->can('delete', $model);
     }
