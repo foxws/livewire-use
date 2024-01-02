c8111291d54feb46846d0265253bb4325a9a7edc
francoism90 <francoism90@users.noreply.github.com>
2024-01-02 20:04:36 +0100

    wip

--- src/Models/Concerns/WithAuthorization.php
+++ src/Models/Concerns/WithAuthorization.php
@@ -6,34 +6,31 @@ use Illuminate\Database\Eloquent\Model;
 
 trait WithAuthorization
 {
-    protected function can(string $ability, ?Model $model = null): void
+    protected function canViewAny(Model|string $model): void
     {
-        abort_if(! $model && ! property_exists(static::class, 'model'), 403);
+        if ($model instanceof Model) {
+            $model = $model->getMorphClass();
+        }
 
-        $this->authorize($ability, $model ?? static::$model);
+        $this->authorize('viewAny', $model);
     }
 
-    protected function canViewAny(?Model $model = null): void
-    {
-        $this->can('viewAny', $model);
-    }
-
-    protected function canView(?Model $model = null): void
+    protected function canView(Model $model): void
     {
         $this->can('viewAny', $model);
     }
 
-    protected function canCreate(?Model $model = null): void
+    protected function canCreate(Model $model): void
     {
         $this->can('create', $model);
     }
 
-    protected function canUpdate(?Model $model = null): void
+    protected function canUpdate(Model $model): void
     {
         $this->can('update', $model);
     }
 
-    protected function canDelete(?Model $model = null): void
+    protected function canDelete(Model $model): void
     {
         $this->can('delete', $model);
     }
