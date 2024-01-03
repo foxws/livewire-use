cb7f2b6b2547cb11b2af77d9eb4532fb54e6352d
francoism90 <francoism90@users.noreply.github.com>
2024-01-03 12:46:42 +0100

    wip

--- src/Views/Concerns/WithForms.php
+++ src/Views/Concerns/WithForms.php
@@ -14,4 +14,31 @@ trait WithForms
             ? $forms->first()
             : $forms->firstWhere('propertyName', $name);
     }
+
+    protected function getFormValue(string $name, ?string $form = null): mixed
+    {
+        if (! $form = $this->getForm($form)) {
+            return null;
+        }
+
+        return $form->getPropertyValue($name);
+    }
+
+    protected function getFormValues(?string $form = null): mixed
+    {
+        if (! $form = $this->getForm($form)) {
+            return null;
+        }
+
+        return $form->all();
+    }
+
+    protected function hasFormProperty(string $name, ?string $form = null): mixed
+    {
+        if (! $form = $this->getForm($form)) {
+            return null;
+        }
+
+        return $form->hasProperty($name);
+    }
 }
