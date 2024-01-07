e49b07d927db96cb7a2ca1340b55e22f57779462
francoism90 <francoism90@users.noreply.github.com>
2024-01-07 12:35:39 +0100

    wip

--- src/Auth/Controllers/LoginController.php
+++ src/Auth/Controllers/LoginController.php
@@ -3,14 +3,8 @@
 namespace Foxws\LivewireUse\Controllers;
 
 use Foxws\LivewireUse\Views\Components\Page;
-use Illuminate\View\View;
 
 class LoginController extends Page
 {
-    protected static string $view = 'ui.container';
-
-    public function render(): View
-    {
-
-    }
+    protected static string $view = 'auth.login';
 }
