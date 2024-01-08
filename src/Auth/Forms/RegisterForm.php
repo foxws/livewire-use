a73e663ad2c21e2f721383beaaaaccf650699991
francoism90 <francoism90@users.noreply.github.com>
2024-01-08 10:30:27 +0100

    wip

---
+++ src/Auth/Forms/RegisterForm.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Foxws\LivewireUse\Auth\Forms;
+
+use Foxws\LivewireUse\Forms\Components\Form;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
+use Livewire\Attributes\Validate;
+
+class RegisterForm extends Form
+{
+    protected static int $maxAttempts = 5;
+
+    #[Validate]
+    public ?string $email = null;
+
+    #[Validate]
+    public ?string $password = null;
+
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email',
+            'password' => [
+                'required',
+                Password::defaults(),
+            ],
+        ];
+    }
+
+    protected function handle()
+    {
+        if (Auth::attempt($this->all())) {
+            session()->regenerate();
+
+            return redirect()->intended();
+        }
+
+        $this->addError('email', __('These credentials do not match our records'));
+    }
+}
