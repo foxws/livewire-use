c8e6cc78a53a7f1e535adb9c6a24d6311771dea5
francoism90 <francoism90@users.noreply.github.com>
2024-01-09 11:43:07 +0100

    wip

--- src/Facades/LivewireUse.php
+++ src/Facades/LivewireUse.php
@@ -2,17 +2,7 @@
 
 namespace Foxws\LivewireUse\Facades;
 
-use Foxws\LivewireUse\Auth\Controllers\LoginController;
-use Foxws\LivewireUse\Auth\Controllers\LogoutController;
-use Foxws\LivewireUse\Auth\Controllers\RegisterController;
-use Foxws\LivewireUse\Support\Discover\ComponentScout;
-use Foxws\LivewireUse\Support\Discover\LivewireScout;
-use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Facade;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Stringable;
-use Livewire\Livewire;
-use Spatie\StructureDiscoverer\Data\DiscoveredClass;
 
 /**
  * @see \Foxws\LivewireUse\LivewireUse
@@ -22,60 +12,5 @@ class LivewireUse extends Facade
     protected static function getFacadeAccessor()
     {
         return \Foxws\LivewireUse\LivewireUse::class;
-    }
-
-    public static function routes(): void
-    {
-        Route::name('auth.')->group(function () {
-            Route::get('/login', LoginController::class)->name('login');
-            Route::get('/logout', LogoutController::class)->name('logout');
-            Route::get('/register', RegisterController::class)->name('register');
-        });
-    }
-
-    public static function registerComponents(string $path, ?string $prefix = null): void
-    {
-        $components = ComponentScout::create()
-            ->path($path)
-            ->prefix($prefix)
-            ->get();
-
-        collect($components)
-            ->each(function (DiscoveredClass $class) {
-                $name = static::componentName($class);
-
-                Blade::component($class->getFcqn(), $name->value());
-            });
-    }
-
-    public static function registerLivewireComponents(string $path, ?string $prefix = null): void
-    {
-        $components = LivewireScout::create()
-            ->path($path)
-            ->prefix($prefix)
-            ->get();
-
-        collect($components)
-            ->each(function (DiscoveredClass $class) {
-                $name = static::componentName($class);
-
-                Livewire::component($name->value(), $class->getFcqn());
-            });
-    }
-
-    public static function componentPrefix(DiscoveredClass $class, string $app = 'App\\'): Stringable
-    {
-        return str($class->namespace)
-            ->after($app)
-            ->match('/(.*)\\\\/')
-            ->kebab()
-            ->finish('-');
-    }
-
-    public static function componentName(DiscoveredClass $class): Stringable
-    {
-        return str($class->name)
-            ->kebab()
-            ->prepend(static::componentPrefix($class));
     }
 }
--- src/LivewireUse.php
+++ src/LivewireUse.php
@@ -2,7 +2,72 @@
 
 namespace Foxws\LivewireUse;
 
+use Foxws\LivewireUse\Auth\Controllers\LoginController;
+use Foxws\LivewireUse\Auth\Controllers\LogoutController;
+use Foxws\LivewireUse\Auth\Controllers\RegisterController;
+use Foxws\LivewireUse\Support\Discover\ComponentScout;
+use Foxws\LivewireUse\Support\Discover\LivewireScout;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Stringable;
+use Livewire\Livewire;
+use Spatie\StructureDiscoverer\Data\DiscoveredClass;
+
+
 class LivewireUse
 {
-    //
+    public static function routes(): void
+    {
+        Route::name('auth.')->group(function () {
+            Route::get('/login', LoginController::class)->name('login');
+            Route::get('/logout', LogoutController::class)->name('logout');
+            Route::get('/register', RegisterController::class)->name('register');
+        });
+    }
+
+    public static function registerComponents(string $path, ?string $prefix = null): void
+    {
+        $components = ComponentScout::create()
+            ->path($path)
+            ->prefix($prefix)
+            ->get();
+
+        collect($components)
+            ->each(function (DiscoveredClass $class) {
+                $name = static::componentName($class);
+
+                Blade::component($class->getFcqn(), $name->value());
+            });
+    }
+
+    public static function registerLivewireComponents(string $path, ?string $prefix = null): void
+    {
+        $components = LivewireScout::create()
+            ->path($path)
+            ->prefix($prefix)
+            ->get();
+
+        collect($components)
+            ->each(function (DiscoveredClass $class) {
+                $name = static::componentName($class);
+
+                Livewire::component($name->value(), $class->getFcqn());
+            });
+    }
+
+    public static function componentPrefix(DiscoveredClass $class, string $app = 'App\\'): Stringable
+    {
+        return str($class->namespace)
+            ->after($app)
+            ->match('/(.*)\\\\/')
+            ->kebab()
+            ->finish('-');
+    }
+
+    public static function componentName(DiscoveredClass $class): Stringable
+    {
+        return str($class->name)
+            ->kebab()
+            ->prepend(static::componentPrefix($class));
+    }
 }
