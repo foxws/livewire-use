04109f2170bd91a58cb7114f43b1f574910fb858
francoism90 <francoism90@users.noreply.github.com>
2024-02-27 12:55:50 +0100

    Convert to component

--- src/Ui/Components/Button.php
+++ src/Ui/Components/Button.php
@@ -2,15 +2,17 @@
 
 namespace Foxws\LivewireUse\Ui\Components;
 
+use Foxws\LivewireUse\Views\Components\Component;
 use Illuminate\Contracts\Support\Htmlable;
 
-class Button extends Link
+class Button extends Component
 {
     protected static string $view = 'ui.button';
 
-    public string|Htmlable|null $label = null;
-
-    public string|Htmlable|null $prepend = null;
-
-    public string|Htmlable|null $append = null;
+    public function __construct(
+        public string|Htmlable|null $label = null,
+        public string|Htmlable|null $prepend = null,
+        public string|Htmlable|null $append = null,
+    )
+    {}
 }
