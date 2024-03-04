fd6fbea950e5c44a974e4af814f723fb27b4d079
francoism90 <francoism90@users.noreply.github.com>
2024-03-04 18:43:58 +0100

    wip

--- resources/views/ui/button.blade.php
+++ resources/views/ui/button.blade.php
@@ -1,15 +1,11 @@
 <button
     wire:key="{{ $hash() }}"
-    {{ $attributes
-        ->class([
-            'inline-flex w-full shrink-0 cursor-pointer select-none flex-wrap items-center justify-center' => ! $attributes->twHas('layer'),
-            '!bg-gray-300 cursor-not-allowed opacity-50' => $attributes->has('disabled') && ! $attributes->twHas('color'),
-        ])
-        ->merge([
+    {{ $attributes->class([
+            'inline-flex w-full shrink-0 cursor-pointer select-none flex-wrap items-center justify-center' => !$attributes->twHas('layer'),
+            '!bg-gray-300 pointer-events-none opacity-50' => $attributes->has('disabled') && !$attributes->twHas('color'),
+        ])->merge([
             'type' => 'button',
-        ])
-        ->twMerge('layer', 'color', 'disabled')
-    }}
+        ])->twMerge('layer', 'color', 'disabled') }}
 >
     {{ $prepend }}
 
