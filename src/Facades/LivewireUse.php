31922355a92ae3734fb922c9bb7f9ff847ef1449
francoism90 <francoism90@users.noreply.github.com>
2024-03-20 15:12:07 +0100

    Add methods

--- src/Facades/LivewireUse.php
+++ src/Facades/LivewireUse.php
@@ -6,6 +6,13 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @see \Foxws\LivewireUse\LivewireUse
+ *
+ * @method static void routes()
+ * @method static void registerComponents(string $name, string $namespace, string $prefix, ?Closure $callback = null)
+ * @method static void registerLivewireComponents(string $name, string $namespace, string $prefix, ?Closure $callback = null)
+ * @method static Stringable componentName(DiscoveredClass $class, string $namespace, string $prefix)
+ * @method static Stringable componentNamespace(DiscoveredClass $class, string $namespace)
+ * @method static string componentPrefix(string $prefix)
  */
 class LivewireUse extends Facade
 {
