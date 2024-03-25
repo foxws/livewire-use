b60eaaaa5a5b78c25deb81af5f7c4832a0cedd0c
francoism90 <francoism90@users.noreply.github.com>
2024-03-25 12:31:30 +0100

    Add auth middlewares

--- src/LivewireUse.php
+++ src/LivewireUse.php
@@ -19,8 +19,8 @@ class LivewireUse
     public static function routes(): void
     {
         Route::name('auth.')->group(function () {
-            Route::get('/login', LoginController::class)->name('login');
-            Route::get('/register', RegisterController::class)->name('register');
+            Route::get('/login', LoginController::class)->middleware('guest')->name('login');
+            Route::get('/register', RegisterController::class)->middleware('guest')->name('register');
             Route::post('/logout', LogoutController::class)->name('logout');
         });
     }
