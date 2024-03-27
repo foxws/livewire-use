ef899e48b95069cc84b52d42bbd319755d0d79de
francoism90 <francoism90@users.noreply.github.com>
2024-03-27 20:01:53 +0100

    wip

--- tests/src/Support/BladeDirectiveTest.php
+++ tests/src/Support/BladeDirectiveTest.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Blade;
 
 uses(TestCase::class);
 
-it('provides a blade directive to merge tailwind classes', function () {
-    expect(Blade::render('<x-livewire-use::actions-button />'))
-        ->toContain('class="w-10 h-10 rounded-full bg-blue-500"')
+it('provides a blade directive to merge classes', function () {
+    expect(Blade::render('<x-test-component />'))
+        ->toContain('class="flex flex-nowrap bg-gray-300 opacity-50"')
         ->toMatchSnapshot();
 });
