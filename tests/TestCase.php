bd0c25acf358fb137a723bf7cf0d6ac2055309ea
francoism90 <francoism90@users.noreply.github.com>
2024-03-27 15:33:55 +0100

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,15 +7,25 @@ use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
+    protected function getEnvironmentSetUp($app)
+    {
+        // Resources
+        $app['config']->set('view.paths', [__DIR__.'/resources/views']);
+
+        // Databases
+        $app['config']->set('database.default', 'testbench');
+
+        $app['config']->set('database.connections.testbench', [
+            'driver'   => 'sqlite',
+            'database' => ':memory:',
+            'prefix'   => '',
+        ]);
+    }
+
     protected function getPackageProviders($app)
     {
         return [
             LivewireUseServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        $app['config']->set('view.paths', [__DIR__.'/resources/views']);
     }
 }
