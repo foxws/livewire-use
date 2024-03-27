0913fe887f21c39cbf706c54abe0e77ac2b37f24
francoism90 <francoism90@users.noreply.github.com>
2024-03-27 18:08:27 +0100

    wip

--- tests/src/Support/Livewire/ModelSynthTest.php
+++ tests/src/Support/Livewire/ModelSynthTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Foxws\LivewireUse\Support\Livewire\Models\ModelSynth;
 use Foxws\LivewireUse\Tests\Models\Post;
 use Foxws\LivewireUse\Tests\TestCase;
 use Livewire\Component;
@@ -8,10 +9,11 @@ use Livewire\Livewire;
 uses(TestCase::class);
 
 beforeEach(function () {
+    Livewire::propertySynthesizer(ModelSynth::class);
     Livewire::component(EditPost::class);
 });
 
-it('can use model route key', function () {
+it('can overrule model synthesizer', function () {
     $post = Post::factory()->create();
 
     Livewire::test(EditPost::class, compact('post'))
