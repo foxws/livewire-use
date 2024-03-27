e9729475495c794652ff0ad4b7a432cfa64df537
francoism90 <francoism90@users.noreply.github.com>
2024-03-27 17:09:05 +0100

    wip

--- tests/src/Models/Post.php
+++ tests/src/Models/Post.php
@@ -24,4 +24,9 @@ class Post extends Model
     {
         return $this->belongsTo(User::class, 'author_id');
     }
+
+    public function getRouteKeyName(): string
+    {
+        return 'uuid';
+    }
 }
